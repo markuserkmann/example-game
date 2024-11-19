a5cd9a8bb06c79c012f8e3208c29273890e2cef0
markuserkmann <markus.erkmann@voco.ee>
2024-11-19 14:16:21 +0200

    first commit

---
+++ game.py
@@ -0,0 +1,12 @@
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode([600, 500])
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
